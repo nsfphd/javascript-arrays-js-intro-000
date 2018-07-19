9b19fcea9847e2b8bdf68b492b7d38c798261b28
Noelle Foster <uufarmgirl@gmail.com>
2018-07-19 16:35:34 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,41 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(myArray, myElement){
+  var newArray=[myElement, ...myArray]
+  return newArray
+}
+
+function addElementToEndOfArray(myArray, myElement){
+  var newArray=[...myArray, myElement]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(myArray, myElement){
+   return myArray.unshift(myElement)
+}
+
+function destructivelyAddElementToEndOfArray(myArray, myElement){
+  return myArray.push(myElement)
+}
+
+function accessElementInArray(myArray, ix){
+  return myArray[ix]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(myArray){
+  return myArray.shift()
+}
+
+function removeElementFromBeginningOfArray(myArray){
+  var newArray=myArray.slice(1)
+  return newArray
+}
+
+function destructivelyRemoveElementFromEndOfArray(myArray){
+  return myArray.pop()
+}
+
+function removeElementFromEndOfArray(myArray){
+  var newArray=myArray.slice(0, myArray.length-1)
+  return newArray
+}
